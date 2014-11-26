b1902d761a6d260538ab095da1b70bdc2dd9bc4b
lzclass <63993048@qq.com>
2014-11-26 21:43:10 +0800

    测试
    Signed-off-by: lzclass <63993048@qq.com>

--- HiSite/src/com/android/hisite/MainActivity.java
+++ HiSite/src/com/android/hisite/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		Log.v("test", "他才是刘昭");
+		Log.v("test2", "家里电脑");
 		return true;
 	}
 
