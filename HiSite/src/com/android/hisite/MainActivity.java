968ce699b267f40264ba4c2cf6208a802b9f4ae7
lz <63993048@qq.com>
2014-11-27 09:39:55 +0800

    公司电脑测试

--- HiSite/src/com/android/hisite/MainActivity.java
+++ HiSite/src/com/android/hisite/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		Log.v("test", "他才是刘昭");
 		Log.v("test2", "家里电脑");
+		Log.v("test2", "公司电脑");
 		return true;
 	}
 
