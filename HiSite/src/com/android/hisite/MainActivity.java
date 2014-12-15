28be447db0e71ca7e1b65c0c84ae446e3f26391c
tcmaster <18600416253@163.com>
2014-12-15 18:24:58 +0800

    jj

--- HiSite/src/com/android/hisite/MainActivity.java
+++ HiSite/src/com/android/hisite/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		Log.v("test", "他才是刘昭");
-		Log.v("test2", "家里电脑");
+		Log.v("test2", "家里电");
 		Log.v("test2", "公司电脑");
 		return true;
 	}
