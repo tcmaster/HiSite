981dee8a2d6c67f1792e5c0a8850a096fdece222
lz <63993048@qq.com>
2014-12-30 17:42:19 +0800

    提交发活动页

--- HiSite/src/com/android/hisite/fragment/PostEventsFragment.java
+++ HiSite/src/com/android/hisite/fragment/PostEventsFragment.java
@@ -1,11 +1,9 @@
 package com.android.hisite.fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,14 +11,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ImageView.ScaleType;
 
 import com.android.hisite.R;
 import com.android.hisite.activity.ForgetIDOrPwdActivity;
-import com.android.utils.StringUtils;
-import com.lidroid.xutils.BitmapUtils;
 import com.lidroid.xutils.ViewUtils;
-import com.lidroid.xutils.util.LogUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
@@ -31,7 +25,7 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 public class PostEventsFragment extends Fragment implements OnClickListener {
 
 	/** 主布局 */
-	private View rootView;
+	private View rootView = null;
 	/** 滑动的图片viewpager控件 */
 	@ViewInject(R.id.vp_header_events)
 	private ViewPager viewPager;
@@ -47,6 +41,21 @@ public class PostEventsFragment extends Fragment implements OnClickListener {
 	/** 忘记密码 */
 	@ViewInject(R.id.tv_forgot_pwd)
 	private TextView tv_forgot_pwd;
+	/** 商店头像 */
+	@ViewInject(R.id.iv_postevents_title)
+	private ImageView iv_postevents_title;
+	/** 商店名称 */
+	@ViewInject(R.id.tv_shopname)
+	private TextView tv_shopname;
+	/** 商店状态 */
+	@ViewInject(R.id.tv_shop_state)
+	private TextView tv_shop_state;
+	/** 商店地点 */
+	@ViewInject(R.id.tv_postevents_place)
+	private TextView tv_postevents_place;
+
+	/** 是否登录 */
+	private boolean isLogin = true;
 
 	/** 创建一个静态的实例 */
 	public static PostEventsFragment newInstance() {
@@ -55,55 +64,28 @@ public class PostEventsFragment extends Fragment implements OnClickListener {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
-		/* 主布局初始化 */
-		if (rootView != null) {
-			return rootView;
+		if (isLogin) {
+			rootView = inflater.inflate(R.layout.fragment_post_events_afterlogin, container, false);
+		} else {
+			rootView = inflater.inflate(R.layout.fragment_post_events, container, false);
 		}
-		rootView = inflater.inflate(R.layout.fragment_post_events, container,
-				false);
+
 		ViewUtils.inject(this, rootView); // 注入view和事件
 		initView();
 		return rootView;
 	}
 
 	private void initView() {
-		btn_shop_login.setOnClickListener(this);
-		btn_shop_register.setOnClickListener(this);
-	}
-
-	/**
-	 * 构造ViewPage页面
-	 * 
-	 * @param context
-	 * @param imgResId
-	 * @return
-	 */
-	private ImageView getPageView(final Context context, int imgResId,
-			String imgUrl) {
-
-		DisplayMetrics dm = new DisplayMetrics();
-		getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-		LogUtils.i("width="
-				+ dm.widthPixels
-				+ "---"
-				+ getActivity().getWindowManager().getDefaultDisplay()
-						.getWidth());
-		imgUrl = "http://bbs.lidroid.com/static/image/common/logo.png";
-		ImageView iv = new ImageView(context);
-		iv.setScaleType(ScaleType.FIT_XY);
-		iv.setImageResource(R.drawable.ic_launcher);
-		if (!StringUtils.isNullOrEmpty(imgUrl)) {
-			BitmapUtils bitmapUtils = new BitmapUtils(getActivity());
-			// 加载网络图片
-			bitmapUtils.display(iv, imgUrl);
-		} else {
-			iv.setImageResource(imgResId);// 不可拉伸图片
+		if (isLogin) {
+			tv_shopname.setText("可乐屏新一代餐饮系统");
+			tv_shop_state.setText("发布：12 关注：15");
+			tv_postevents_place.setText("北京 朝阳区");
+		}else {
+			btn_shop_login.setOnClickListener(this);
+			btn_shop_register.setOnClickListener(this);
 		}
-
-		return iv;
 	}
 
 	@Override
@@ -119,14 +101,16 @@ public class PostEventsFragment extends Fragment implements OnClickListener {
 		case R.id.tv_forgot_id:
 			intent = new Intent(getActivity(), ForgetIDOrPwdActivity.class);
 			intent.putExtra("forgottype", 0);
+			startActivity(intent);
 			break;
 		case R.id.tv_forgot_pwd:
 			intent = new Intent(getActivity(), ForgetIDOrPwdActivity.class);
 			intent.putExtra("forgottype", 1);
+			startActivity(intent);
 			break;
 		default:
 			break;
 		}
-		startActivity(intent);
+
 	}
 }
