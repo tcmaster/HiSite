ab9c69af3265e4036da02cc79d41c0d558356322
asus <asus@liuzhao>
2015-01-13 21:42:33 +0800

    活動現場存儲

---
+++ HiSite/src/com/android/tonight8/storage/live/LiveStorage.java
@@ -0,0 +1,23 @@
+package com.android.tonight8.storage.live;
+
+
+/**
+ * @author liuzhao
+ * 活动现场数据存储
+ */
+public class LiveStorage {
+	/**
+	 * 现场列表
+	 */
+	public static void liveListDB() {
+		
+	}
+	/**
+	 * 现场详情
+	 */
+	public static void liveDetailDB() {
+		
+	}
+
+
+}
