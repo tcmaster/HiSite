5a9fd3b23968b106cc032f383c752c8214e7de6c
tcmaster <Administrator@Sc-201411102119.mshome.net>
2015-02-03 22:36:37 +0800

    增加了一些方法

--- HiSite/src/com/android/tonight8/storage/user/UserExchangeNativeController.java
+++ HiSite/src/com/android/tonight8/storage/user/UserExchangeNativeController.java
@@ -11,6 +11,7 @@ import com.android.tonight8.storage.DBUtil;
 import com.android.tonight8.storage.entity.EventEntity;
 import com.android.tonight8.storage.entity.ExchangeEntity;
 import com.android.tonight8.storage.entity.OrgEntity;
+import com.lidroid.xutils.db.sqlite.WhereBuilder;
 
 /**
  * 
@@ -30,31 +31,43 @@ public class UserExchangeNativeController {
 		ExchangeEntity exchangeEntity = new ExchangeEntity();
 		List<OrgEntity> orgEntities = new ArrayList<OrgEntity>();
 		EventEntity eventEntity = new EventEntity();
-		DBUtil.copyData(Exchange.class, ExchangeEntity.class, model.exchange, exchangeEntity);
+		DBUtil.copyData(Exchange.class, ExchangeEntity.class, model.exchange,exchangeEntity);
 		DBUtil.copyData(Event.class, EventEntity.class, model.event, eventEntity);
-		// exchangeEntity.event = eventEntity;
+		exchangeEntity.event = eventEntity;
 		for (int i = 0; i < model.orgs.size(); i++) {
 			OrgEntity orgEntity = new OrgEntity();
 			DBUtil.copyData(Org.class, OrgEntity.class, model.orgs.get(i), orgEntity);
 			orgEntities.add(orgEntity);
 		}
-		exchangeEntity.orgs = orgEntities;
-		DBUtil.saveOrUpdate(exchangeEntity);
-		DBUtil.saveOrUpdate(eventEntity);
-		DBUtil.saveOrUpdateAll(orgEntities);
+		DBUtil.saveOrUpdate(exchangeEntity,ExchangeEntity.class,WhereBuilder.b("rid","=",eventEntity.getId()),"method","address","orgAll","rid");
+		DBUtil.saveOrUpdate(eventEntity,Event.class,"name","timeRangeStart","timeRangeEnd");
+		DBUtil.saveOrUpdateAll(orgEntities,OrgEntity.class,"name","address");
 	}
 
 	/**
-	 * 根据用户和活动获取用户中奖码兑换详情
+	 * 根据活动获取用户中奖码兑换详情
 	 * 
 	 * @param eventId
 	 *            活动id
-	 * @param userId
-	 *            哪个用户(得不到，暂停）
 	 */
-	public UserExchangeModel selectData(long eventId, long userId) {
+	public UserExchangeModel selectData(long eventId) {
 		UserExchangeModel model = new UserExchangeModel();
-
+		EventEntity eventEntity = DBUtil.getDataFirst(EventEntity.class,"id = "+ eventId);
+		ExchangeEntity exchangeEntity = DBUtil.getDataFirst(ExchangeEntity.class, "rid = " +eventId);
+		List<OrgEntity> orgEntities = DBUtil.getData(OrgEntity.class,"join bindTree on org.id = bindTree.parentId where org.id = "+ eventEntity.org.getId());
+		Event event = new Event();
+		Exchange exchange = new Exchange();
+		DBUtil.copyData(EventEntity.class, Event.class,eventEntity,event);
+		DBUtil.copyData(ExchangeEntity.class, Exchange.class,exchangeEntity,exchange);
+		List<Org> orgs = new ArrayList<Org>();
+		for(int i = 0;i < orgEntities.size();i++){
+			Org org = new Org();
+			DBUtil.copyData(OrgEntity.class,Org.class,orgEntities.get(i),org);
+			orgs.add(org);
+		}
+		model.event = event;
+		model.exchange = exchange;
+		model.orgs = orgs;
 		return model;
 	}
 }
