3c911c7f0f1d2547038a86bcdcb87aa8e0efa0c9
asus <asus@liuzhao>
2015-02-05 22:38:19 +0800

    商家绑定表增加双主键

--- HiSite/src/com/android/tonight8/storage/entity/BindEntity.java
+++ HiSite/src/com/android/tonight8/storage/entity/BindEntity.java
@@ -2,42 +2,48 @@ package com.android.tonight8.storage.entity;
 
 import com.lidroid.xutils.db.annotation.Column;
 import com.lidroid.xutils.db.annotation.Foreign;
+import com.lidroid.xutils.db.annotation.Id;
 import com.lidroid.xutils.db.annotation.Table;
+
 /**
  * @Descripton 商家绑定表
  * @author LiXiaoSong
  * @2015-1-29
  * @Tonight8
  */
-@Table(name="bind")
-public class BindEntity extends BaseEntity{
-	/**绑定的状态*/
-	@Column(column="status")
+@Table(name = "bind")
+public class BindEntity extends BaseEntity {
+	/** 绑定的状态 */
+	@Column(column = "status")
 	private int status;
-	/**绑定的日期*/
-	@Column(column="date")
+	/** 绑定的日期 */
+	@Column(column = "date")
 	private String date;
-	/**绑定的商家*/
-	@Foreign(column="rid",foreign="id")
+	/** 绑定的商家 */
+	@Id
+	@Foreign(column = "rid", foreign = "id")
 	public OrgEntity org;
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	@Override
 	public String toString() {
 		return "BindEntity [status=" + status + ", date=" + date + ", org="
 				+ org + "]";
 	}
-	
-	
-	
+
 }
