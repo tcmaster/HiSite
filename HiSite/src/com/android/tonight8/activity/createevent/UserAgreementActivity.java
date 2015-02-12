386efa4527ff0874dfa426a3789604a4359770aa
asus <asus@liuzhao>
2015-02-12 22:31:09 +0800

    更新界面

--- HiSite/src/com/android/tonight8/activity/createevent/UserAgreementActivity.java
+++ HiSite/src/com/android/tonight8/activity/createevent/UserAgreementActivity.java
@@ -3,13 +3,16 @@ package com.android.tonight8.activity.createevent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
 import com.android.tonight8.R;
 import com.android.tonight8.base.BaseActivity;
+import com.android.tonight8.view.WebViewForScrollView;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.lidroid.xutils.view.annotation.event.OnCompoundButtonCheckedChange;
@@ -23,7 +26,7 @@ public class UserAgreementActivity extends BaseActivity {
 
 	/** 协议网页 */
 	@ViewInject(R.id.wv_user_agreement)
-	private WebView wv_user_agreement;
+	private WebViewForScrollView wv_user_agreement;
 	/** 选中同意 */
 	@ViewInject(R.id.cb_agree)
 	private CheckBox cb_agree;
@@ -31,7 +34,7 @@ public class UserAgreementActivity extends BaseActivity {
 	@ViewInject(R.id.btn_user_agreement)
 	private Button btn_user_agreement;
 	/** url地址 */
-	private String agreementUrl="http://www.baidu.com";
+	private String agreementUrl = "http://www.baidu.com";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,26 @@ public class UserAgreementActivity extends BaseActivity {
 	}
 
 	private void initData() {
+		WebSettings webSettings = wv_user_agreement.getSettings();
+		// 设置WebView属性，能够执行Javascript脚本
+		webSettings.setJavaScriptEnabled(true);
+		// 设置可以访问文件
+		webSettings.setAllowFileAccess(true);
+		// 设置支持缩放
+		webSettings.setBuiltInZoomControls(true);
+
+		// 设置Web视图
+		wv_user_agreement.setWebViewClient(new webViewClient());
+		// 加载需要显示的网页
 		wv_user_agreement.loadUrl(agreementUrl);
+	}
+
+	// Web视图
+	private class webViewClient extends WebViewClient {
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
 	}
 
 	@OnClick(R.id.btn_user_agreement)
@@ -49,7 +71,8 @@ public class UserAgreementActivity extends BaseActivity {
 		if (!cb_agree.isChecked()) {
 			return;
 		}
-		startActivityForAnima(new Intent(UserAgreementActivity.this,CreatEventFirstActivity.class), null);
+		startActivityForAnima(new Intent(UserAgreementActivity.this,
+				CreatEventFirstActivity.class), null);
 
 	}
 
