c7dcf72c3d8cc70f83f323dc7f3728b512d3e44f
tcmaster <18600416253@163.com>
2015-02-15 17:26:33 +0800

    加了个布局

---
+++ HiSite/src/com/android/tonight8/activity/user/UserAddressActivity.java
@@ -0,0 +1,16 @@
+/**
+ * 2015-2-15
+ */
+package com.android.tonight8.activity.user;
+
+import com.android.tonight8.base.BaseActivity;
+
+/**
+ * @Description:用户中奖管理界面
+ * @author:LiXiaoSong
+ * @copyright @HiSite
+ * @Date:2015-2-15
+ */
+public class UserAddressActivity extends BaseActivity {
+
+}
