44ed48173d07061b5cd16f6df5b836ddab618c8b
tcmaster <18600416253@163.com>
2015-02-25 09:47:21 +0800

    节前的东西，好像没提

--- HiSite/src/com/android/tonight8/activity/user/UserAddressActivity.java
+++ HiSite/src/com/android/tonight8/activity/user/UserAddressActivity.java
@@ -3,6 +3,9 @@
  */
 package com.android.tonight8.activity.user;
 
+import android.os.Bundle;
+
+import com.android.tonight8.R;
 import com.android.tonight8.base.BaseActivity;
 
 /**
@@ -13,4 +16,9 @@ import com.android.tonight8.base.BaseActivity;
  */
 public class UserAddressActivity extends BaseActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_only_list);
+		super.onCreate(savedInstanceState);
+	}
 }
