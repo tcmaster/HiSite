d82c8713b56b07f575141fd196afb6b1c5d2cd57
tcmaster <18600416253@163.com>
2015-03-10 17:36:55 +0800

    多余的提交

---
+++ HiSite/src/com/android/tonight8/activity/user/UserForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.android.tonight8.activity.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.tonight8.R;
+
+public class UserForgetPasswordActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_forget_password);
+	}
+
+}
