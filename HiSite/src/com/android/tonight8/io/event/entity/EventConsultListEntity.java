963f386ff46cc9ee82f6a28e08dd642f251c1d3b
tcmaster <Administrator@Sc-201411102119>
2015-03-16 18:55:53 +0800

    dd

---
+++ HiSite/src/com/android/tonight8/io/event/entity/EventConsultListEntity.java
@@ -0,0 +1,10 @@
+package com.android.tonight8.io.event.entity;
+
+import java.util.List;
+
+import com.android.tonight8.io.net.NetEntityBase;
+import com.android.tonight8.model.event.EventConsultModel;
+
+public class EventConsultListEntity extends NetEntityBase {
+	List<EventConsultModel> eventPublishConsults;
+}
